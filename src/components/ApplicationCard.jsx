f3af0de577902a767add51d04564a8785d648b83
JahnaviGundam <jahnavigundam6@gmail.com>
2026-06-14 22:14:48 +0530

    commit on feature

---
+++ src/components/ApplicationCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ApplicationCard() {
+  return (
+    //testing revert
+    <div>
+        <h1>Application Card</h1>
+    </div>
+  )
+}
+
+export default ApplicationCard
