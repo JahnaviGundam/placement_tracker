dbbec219b0ad84520015664775ee5bcb7e5ec742
JahnaviGundam <jahnavigundam6@gmail.com>
2026-06-14 23:07:08 +0530

    Revert "delete commit"

---
+++ src/components/ApplicationCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ApplicationCard() {
+  return (
+    //mmerge change
+    <div>
+        <h1>Application Card</h1>
+    </div>
+  )
+}
+
+export default ApplicationCard
+
